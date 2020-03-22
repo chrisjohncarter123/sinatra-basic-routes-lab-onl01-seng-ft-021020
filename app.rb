25e5dfbf9b4b054f1674f108a551f3d20b0b1807
Christopher Carter <chrisjohncarter123@gmail.com>
2020-03-22 01:16:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,4 +8,12 @@ class App < Sinatra::Base
   get '/name' do
     "My name is Chris"
   end
+  
+   get '/hometown' do
+    "My hometown is St. Louis"
+  end
+  
+   get '/favorite-song' do
+    "My name is Stairway to Heaven"
+  end
 end
